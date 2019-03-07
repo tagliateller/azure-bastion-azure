fba9864b7845846505fd799c38aeb5278f1a577a
John Smith <hotzel-schm@online.de>
2019-03-07 18:03:33 +0100

    git clone added

--- scripts/install.sh
+++ scripts/install.sh
@@ -55,4 +55,24 @@ EOF
 
 chown ${USERNAME} /home/${USERNAME}/.azure/credentials
 
+git clone git@github.com:tagliateller/openshift-origin.git tagliateller-openshift-origin
+cd tagliateller-openshift-origin
+git checkout release-3.11
+
+git clone git@github.com:tagliateller/azure-openshift-svt.git tagliateller-azure-openshift-svt
+cd tagliateller-azure-openshift-svt
+git checkout release-3.11
+
+git clone git@github.com:tagliateller/azure-bastion-van.git tagliateller-azure-bastion-van
+cd tagliateller-azure-bastion-van
+git checkout release-3.11
+
+git clone git@github.com:tagliateller/azure-bastion-kubevirt.git tagliateller-azure-bastion-kubevirt
+cd tagliateller-azure-bastion-kubevirt
+git checkout release-3.11
+
+git clone git@github.com:tagliateller/azure-origin-windows.git tagliateller-azure-origin-windows
+cd tagliateller-azure-origin-windows
+git checkout release-3.11
+
 echo $(date) " - Script complete"
